d004ea774e45da4b6b52eac54630bd5907924122
tclauzure <theo.clauzure@outlook.fr>
2025-05-20 16:33:15 +0200

    fix(drizzle): resolve git review

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,11 +1,16 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
 export default defineConfig({
   out: './migrations',
   schema: './schema',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,7 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
 const db = drizzle(process.env.DATABASE_URL!);

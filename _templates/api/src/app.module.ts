2c244df06ecc473f4ab669ac3cf97da37ee2796d
rharkor <louis@huort.com>
2025-05-20 11:22:51 +0200

    fix: module (#42)

--- _templates/api/src/app.module.ts
+++ _templates/api/src/app.module.ts
@@ -1,23 +1,11 @@
 import { Module } from "@nestjs/common"
-import { ClientsModule, Transport } from "@nestjs/microservices"
 
 import { UserController } from "./users/user.controller"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
-import { MICROSERVICES_CLIENTS } from "./constants"
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: MICROSERVICES_CLIENTS.USERS_SERVICE,
-        transport: Transport.TCP,
-        options: {
-          port: 4003,
-        },
-      },
-    ]),
-  ],
+  imports: [],
   controllers: [AppController, UserController],
   providers: [AppService],
 })

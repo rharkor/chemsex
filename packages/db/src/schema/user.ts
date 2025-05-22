8c263a540b5daed5dc54d34932fa8179720ca006
Thibault5 <thibault.cauche@gmail.com>
2025-05-22 15:57:42 +0200

    fix(bdd):fix schema (#59)

--- packages/db/src/schema/user.ts
+++ packages/db/src/schema/user.ts
@@ -1,7 +1,7 @@
-import { pgTable, text, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, text, serial } from 'drizzle-orm/pg-core';
 
 export const userTable = pgTable('user', {
-  id: varchar('id', { length: 36 }).primaryKey(),
+  id: serial('id').primaryKey(),
   email: text('email').notNull(),
   password: text('password').notNull(),
   username: text('username').notNull(),

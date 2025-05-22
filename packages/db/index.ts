dd63cb35317d3420fa358949380bce358cd39e0d
rharkor <louis@huort.com>
2025-05-22 09:29:37 +0200

    fix: drizzle (#50)

--- packages/db/index.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not defined');
-}
-const db = drizzle(process.env.DATABASE_URL!);

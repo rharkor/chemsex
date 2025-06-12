cec6a4102adec129f2f0cb2b1c46ebaaadc8ccc8
tclauzure <theo.clauzure@outlook.fr>
2025-06-12 16:51:53 +0200

    feat(crowdfunding): roots get all (#65)

--- apps/api/gateway/src/crowdfunding/crowdfunding.controller.ts
+++ apps/api/gateway/src/crowdfunding/crowdfunding.controller.ts
@@ -1,7 +1,7 @@
 import { Request } from "express"
 import { MICROSERVICES_CLIENTS } from "src/constants"
 
-import { Body, Controller, Inject, Post, Req } from "@nestjs/common"
+import { Body, Controller, Get, Inject, Post, Req } from "@nestjs/common"
 import { ClientProxy } from "@nestjs/microservices"
 
 @Controller("crowdfunding")
@@ -9,7 +9,7 @@ export class CrowfundingController {
   constructor(
     @Inject(MICROSERVICES_CLIENTS.CROWDFUNDING_SERVICE)
     private readonly crowdfundingServiceClient: ClientProxy
-  ) {}
+  ) { }
 
   @Post("create_campaign")
   createCampaign(@Body() body: unknown, @Req() request: Request) {
@@ -17,5 +17,11 @@ export class CrowfundingController {
       data: body,
       ctx: { token: request.headers.authorization },
     })
+  }
+
+  @Get()
+  getAll() {
+    console.log("getAll called with body:")
+    return this.crowdfundingServiceClient.send("get_all", {})
   }
 }

1c14768a7be6d3b3a29eeaf2c2628365bcd350dd
tclauzure <theo.clauzure@outlook.fr>
2025-06-12 14:16:39 +0200

    feat(crowdfunding): change on dto, attempt on getMe

--- apps/api/crowdfunding/src/crowdfunding/crowdfunding.service.ts
+++ apps/api/crowdfunding/src/crowdfunding/crowdfunding.service.ts
@@ -2,18 +2,22 @@ import { eq } from "drizzle-orm"
 import { Request } from "express"
 import { CreateCrowdfundingDto } from "src/dtos/createCrowdfundingDto"
 
-import { Injectable } from "@nestjs/common"
-import { RpcException } from "@nestjs/microservices"
-import { db } from "@party-n-play/db"
+import { Injectable, UnauthorizedException } from "@nestjs/common"
+import { ClientProxy } from "@nestjs/microservices"
+
 
 
 
 @Injectable()
 export class CrowdfundingService {
-  createCrowdfunding = (createCrowndfundingDto: CreateCrowdfundingDto, request: Request) => {
-    //const { goal, endDate, name, description, image } = createCrowndfundingDto
-    console.log(request);
+  constructor(private readonly usersServiceClient: ClientProxy) { }
+  createCrowdfunding = async (parameters: CreateCrowdfundingDto) => {
+    const user = await this.usersServiceClient.send('getMe', { token: parameters.ctx.token })
+    if (!user) {
+      throw new UnauthorizedException("Utilisateur non authentifié")
+    }
+    console.log(parameters);
 
-    return {"oui": "non"}
+    return { success: true }
   }
 }

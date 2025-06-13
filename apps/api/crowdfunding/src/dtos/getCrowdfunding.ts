7995d52e822995c0a9b6fa25371a91d280ad9ac6
tclauzure <theo.clauzure@outlook.fr>
2025-06-13 16:12:45 +0200

    fix(crowdfunding): paginationn and new dto for getbyid (#65)

--- apps/api/crowdfunding/src/dtos/getCrowdfunding.ts
+++ apps/api/crowdfunding/src/dtos/getCrowdfunding.ts
@@ -4,6 +4,7 @@ export const GetCrowdfundingDto = z.object({
   goal: z.coerce.number().nullish(),
   name: z.string().min(3).max(40).nullish(),
   userId: z.coerce.number().nullish(),
+  page: z.coerce.number().min(1).default(1),
 })
 
 export type GetCrowdfundingDto = z.infer<typeof GetCrowdfundingDto>

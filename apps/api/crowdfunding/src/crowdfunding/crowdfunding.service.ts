c963adfc1a0257072bda4091218c34c8190984a4
tclauzure <theo.clauzure@outlook.fr>
2025-06-17 17:19:08 +0200

    feat(crowdfunding): update crowdfunding roots done (#66)

--- apps/api/crowdfunding/src/crowdfunding/crowdfunding.service.ts
+++ apps/api/crowdfunding/src/crowdfunding/crowdfunding.service.ts
@@ -2,19 +2,20 @@ import { and, eq, SQLWrapper } from "drizzle-orm"
 import { firstValueFrom } from "rxjs"
 import { MICROSERVICES_CLIENTS } from "src/constants"
 import { CreateCrowdfundingDto } from "src/dtos/createCrowdfundingDto"
+import { UpdateCrowdfundingDto } from "src/dtos/updateCrowdfundingDto"
 
 import { Inject, Injectable, UnauthorizedException } from "@nestjs/common"
 import { ClientProxy } from "@nestjs/microservices"
 import { db } from "@party-n-play/db"
 import { crowdfundingTable } from "@party-n-play/db/schemas/crowdfunding"
-import { off } from "process"
+
 
 @Injectable()
 export class CrowdfundingService {
   constructor(
     @Inject(MICROSERVICES_CLIENTS.USERS_SERVICE)
     private readonly usersServiceClient: ClientProxy
-  ) {}
+  ) { }
 
   createCrowdfunding = async (parameters: CreateCrowdfundingDto) => {
     const userObservable = this.usersServiceClient.send("get_me", { token: parameters.ctx.token })
@@ -51,5 +52,20 @@ export class CrowdfundingService {
 
   getById(id: number) {
     return db.select().from(crowdfundingTable).where(eq(crowdfundingTable.id, id))
+  }
+
+  async updateCrowdfunding(parameters: { id: UpdateCrowdfundingDto["id"]; ctx: UpdateCrowdfundingDto["ctx"]; data: UpdateCrowdfundingDto["data"]; }) {
+    const userObservable = this.usersServiceClient.send("get_me", { token: parameters.ctx.token })
+    const user = await firstValueFrom(userObservable)
+    if (!user) {
+      throw new UnauthorizedException("Utilisateur non authentifié")
+    }
+    const { goal, endDate, description, image, name } = parameters.data
+    return db
+      .update(crowdfundingTable)
+      .set({ goal, endDate: new Date(endDate), description, image, name })
+      .where(eq(crowdfundingTable.id, parameters.id))
+      .returning()
+
   }
 }
